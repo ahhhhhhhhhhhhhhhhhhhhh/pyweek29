1bcf1d78cda9db1819130f3eb12882c159afb92f
Starbuck5 <charlie.c.hayden@gmail.com>
2020-03-25 00:33:58 -0700

    Made the resources panel have colors

--- game/resources.py
+++ game/resources.py
@@ -15,18 +15,21 @@ class Resources:
 
         self.food_display = pygame_gui.elements.UILabel(
             manager=manager,
-            relative_rect=pygame.Rect(20, 20, 150, 25),
+            relative_rect=pygame.Rect(10, 10, 218, 40),
             text=f"Food: {self.food}",
+            object_id = "food_display"
         )
         self.population_display = pygame_gui.elements.UILabel(
             manager=manager,
-            relative_rect=pygame.Rect(20, 50, 150, 25),
+            relative_rect=pygame.Rect(238, 10, 296, 40),
             text=f"Population: {self.population}",
+            object_id = "population_display"
         )
         self.territory_display = pygame_gui.elements.UILabel(
             manager=manager,
-            relative_rect=pygame.Rect(20, 80, 150, 25),
+            relative_rect=pygame.Rect(544, 10, 283, 40),
             text=f"Territory: {self.territory}",
+            object_id = "territory_display"
         )
 
     def __setattr__(self, name, value):
