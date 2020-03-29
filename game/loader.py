4210f2249a8bd3673ed1ccfaaba7ca4942e923c4
Starbuck5 <charlie.c.hayden@gmail.com>
2020-03-28 18:23:58 -0700

    MacOS loader compatibility
    Mac.spec creates an app bundle, but it doesn't work out of the box. You have to go in, find the data folder and move it up one directory

--- game/loader.py
+++ game/loader.py
@@ -1,8 +1,12 @@
 import os
+import sys
 
 data_py = os.path.abspath(os.path.dirname(__file__))
 data_dir = os.path.normpath(os.path.join(data_py, "..", "data"))
 
+if getattr(sys, 'frozen', False):
+    data_py = os.path.abspath(os.path.dirname(sys.executable))
+    data_dir = os.path.normpath(os.path.join(data_py, "..", "data"))
 
 def filepath(filename):
     """Determine the path to a file in the data directory.
